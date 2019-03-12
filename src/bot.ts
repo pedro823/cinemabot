8001922c0fab570a551566b0bfd37d0905731545
tiagonapoli <napoli.tiago@hotmail.com>
2019-03-12 15:59:16 -0300

    Add Welcome user

--- src/bot.ts
+++ src/bot.ts
@@ -24,8 +24,14 @@ export class MyBot {
      * @param {TurnContext} context on turn context object.
      */
     public onTurn = async (turnContext: TurnContext) => {
-        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
-        if (turnContext.activity.type !== ActivityTypes.Message) {
+        if (turnContext.activity.type === ActivityTypes.Message) {
+            await turnContext.sendActivity({
+                text: 'Here is an Adaptive Card:',
+                attachments: [CardFactory.adaptiveCard(this.movieCard())]
+            }); 
+        } else if (turnContext.activity.type === ActivityTypes.ConversationUpdate) {
+            this.greetUser(turnContext)
+        } else {
             // Generic handler for all other activity types.
             await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
             return
@@ -46,11 +52,27 @@ export class MyBot {
         return getMovies()
     }
 
+    private async greetUser(turnContext: TurnContext) {
+        await this.sendWelcomeMessage(turnContext);
+        await this.welcomedUserProperty.set(turnContext, true);
+        await this.userState.saveChanges(turnContext);
+    }
+
     private isUserWelcomed(turnContext) {
         return this.welcomedUserProperty.get(turnContext, true);
     }
 
     private movieCard() {
         return MovieCard
+    }
+
+    private async sendWelcomeMessage(turnContext: TurnContext) {
+        if (turnContext.activity.membersAdded.length !== 0) {
+            for (let idx in turnContext.activity.membersAdded) {
+                if (turnContext.activity.membersAdded[idx].id !== turnContext.activity.recipient.id) {
+                    await turnContext.sendActivity(`Bem-vindo ao Cinema Bot! Eu posso te informar os filmes em cartaz na sua cidade e os horários das sessões... O que você gostaria de saber?`);
+                }
+            }
+        }
     }
 }
