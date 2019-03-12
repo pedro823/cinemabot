e30ab59aa86cd9f41c05395f7da31ddb7939419c
Cezar <cezarguimaraes2810@gmail.com>
2019-03-12 15:06:53 -0300

    Crawler beginnin

---
+++ src/showtimes.ts
@@ -0,0 +1,74 @@
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import { writeFileSync } from 'fs';
+import { flatten } from 'ramda';
+
+const url = {
+  movies: (page: number) =>
+    `https://www.cinemark.com.br/sao-paulo/filmes/em-cartaz?pagina=${page}`,
+  showtimes: (path: string) =>
+    `https://www.cinemark.com.br${path}`,
+};
+
+interface Movie {
+  title: string;
+  path: string;
+  rating: number;
+  image: string;
+}
+
+const parseMovies = async (page: number) => {
+  const movies: Movie[] = [];
+
+  const html = await axios.get(url.movies(page));
+
+  const $ = cheerio.load(html.data);
+  const container = $('.movie-container');
+  if (container.length > 0) {
+    container.each((_, element) => {
+      const details = $('.movie-details', element);
+      const title = $('h3', details).text().trim();
+      const path = $('a', details).attr('href');
+      const rating = Number($('.rating-abbr', details).text().trim());
+      const image = $('source', element).attr('srcset');
+      movies.push({
+        title, path, rating, image
+      })
+    });
+  }
+
+  return movies;
+}
+
+const getMovies = async () => {
+  return flatten(await Promise.all([1, 2].map(parseMovies)));
+}
+
+const parseShowtimes = async (path: string) => {
+  const showtimes = []
+  
+  const html = await axios.get(url.showtimes(path));
+
+  const $ = cheerio.load(html.data);
+  const theaters = $('.theater');
+  if (theaters.length > 0) {
+    theaters.each((_, element) => {
+      // const details = $('.movie-details', element);
+      // const title = $('h3', details).text().trim();
+      // const path = $('a', details).attr('href');
+      // const rating = Number($('.rating-abbr', details).text().trim());
+      // const image = $('source', element).attr('srcset');
+      // movies.push({
+      //   title, path, rating, image
+      // })
+
+    });
+  }
+
+  return movies;
+}
+
+getMovies().then(movies => {
+  console.log(movies)
+  console.log(movies.length)
+})
